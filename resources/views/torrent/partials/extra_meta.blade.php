5c416d75d43a136f30719c7edd6ea289fb69b231
Roardom <roardom@protonmail.com>
2025-04-07 08:17:18 +0000

    refactor: add alpine tabs component

--- resources/views/torrent/partials/extra_meta.blade.php
+++ resources/views/torrent/partials/extra_meta.blade.php
@@ -1,46 +1,17 @@
-<section
-    class="panelV2"
-    x-data="{
-        tab: window.location.hash ? window.location.hash.substring(1) : 'recommendations',
-    }"
-    id="tab_wrapper"
->
-    <!-- The tabs navigation -->
+<section class="panelV2" x-data="tabs" data-default-tab="recommendations" id="tab_wrapper">
     <h2 class="panel__heading">Relations</h2>
     <menu class="panel__tabs">
-        <li
-            class="panel__tab"
-            role="tab"
-            x-bind:class="tab === 'recommendations' && 'panel__tab--active'"
-            x-on:click="tab = 'recommendations'; window.location.hash = 'recommendations'"
-        >
-            Recommendations
-        </li>
-        <li
-            class="panel__tab"
-            role="tab"
-            x-bind:class="tab === 'collection' && 'panel__tab--active'"
-            x-on:click="tab = 'collection'; window.location.hash = 'collection'"
-        >
-            Collection
-        </li>
-        <li
-            class="panel__tab"
-            role="tab"
-            x-bind:class="tab === 'playlists' && 'panel__tab--active'"
-            x-on:click="tab = 'playlists'; window.location.hash = 'playlists'"
-        >
-            Playlists
-        </li>
+        <li class="panel__tab" x-bind="tabButton" data-tab="recommendations">Recommendations</li>
+        <li class="panel__tab" x-bind="tabButton" data-tab="collection">Collection</li>
+        <li class="panel__tab" x-bind="tabButton" data-tab="playlists">Playlists</li>
     </menu>
-    <!-- The tabs content -->
-    <div x-show="tab === 'recommendations'">
+    <div x-bind="tabPanel" data-tab="recommendations">
         @include('torrent.partials.recommendations')
     </div>
-    <div x-show="tab === 'collection'" x-cloak>
+    <div x-bind="tabPanel" data-tab="collection" x-cloak>
         @include('torrent.partials.collection')
     </div>
-    <div x-show="tab === 'playlists'" x-cloak>
+    <div x-bind="tabPanel" data-tab="playlists" x-cloak>
         @include('torrent.partials.playlists')
     </div>
 </section>
