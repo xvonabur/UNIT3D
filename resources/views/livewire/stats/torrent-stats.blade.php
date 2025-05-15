9db5a9c45bfbfbdbffd591bb7e4c1d5292cc7eb5
Roardom <roardom@protonmail.com>
2025-05-15 21:44:21 +0000

    update: use nested definitions lists for extra stats
    Looks better and less redundant. However it looks slightly worse when copying as plain text and pasting elsewhere.

--- resources/views/livewire/stats/torrent-stats.blade.php
+++ resources/views/livewire/stats/torrent-stats.blade.php
@@ -1,22 +1,32 @@
 <section class="panelV2 panel--grid-item">
     <h2 class="panel__heading">{{ __('torrent.torrents') }}</h2>
     <dl class="key-value">
-        @foreach ($categories as $category)
-            <div class="key-value__group">
-                <dt>{{ $category->name }} {{ __('common.category') }}</dt>
-                <dd>{{ $category->torrents_count }}</dd>
-            </div>
-        @endforeach
-    </dl>
-    <dl class="key-value">
-        @foreach ($resolutions as $resolution)
-            <div class="key-value__group">
-                <dt>{{ $resolution->name }} {{ __('common.resolution') }}</dt>
-                <dd>{{ $resolution->torrents_count }}</dd>
-            </div>
-        @endforeach
-    </dl>
-    <dl class="key-value">
+        <div class="key-value__group--nested">
+            <dt>{{ __('common.category') }}</dt>
+            <dd>
+                <dl class="key-value">
+                    @foreach ($categories as $category)
+                        <div class="key-value__group">
+                            <dt>{{ $category->name }}</dt>
+                            <dd>{{ $category->torrents_count }}</dd>
+                        </div>
+                    @endforeach
+                </dl>
+            </dd>
+        </div>
+        <div class="key-value__group--nested">
+            <dt>{{ __('common.resolution') }}</dt>
+            <dd>
+                <dl class="key-value">
+                    @foreach ($resolutions as $resolution)
+                        <div class="key-value__group">
+                            <dt>{{ $resolution->name }}</dt>
+                            <dd>{{ $resolution->torrents_count }}</dd>
+                        </div>
+                    @endforeach
+                </dl>
+            </dd>
+        </div>
         <div class="key-value__group">
             <dt>{{ __('stat.total-torrents') }}</dt>
             <dd>{{ $num_torrent }}</dd>
--- resources/views/livewire/stats/user-stats.blade.php
+++ resources/views/livewire/stats/user-stats.blade.php
@@ -2,36 +2,43 @@
     <h2 class="panel__heading">{{ __('common.users') }}</h2>
     <dl class="key-value">
         <div class="key-value__group">
-            <dt>{{ __('stat.all') }} {{ __('common.users') }}</dt>
+            <dt>{{ __('stat.all') }}</dt>
             <dd>{{ $all_user }}</dd>
         </div>
         <div class="key-value__group">
-            <dt>{{ __('stat.active') }} {{ __('common.users') }}</dt>
-            <dd>{{ $active_user }}</dd>
-        </div>
-        <div class="key-value__group">
-            <dt>{{ __('stat.disabled') }} {{ __('common.users') }}</dt>
+            <dt>{{ __('stat.disabled') }}</dt>
             <dd>{{ $disabled_user }}</dd>
         </div>
         <div class="key-value__group">
-            <dt>{{ __('stat.pruned') }} {{ __('common.users') }}</dt>
+            <dt>{{ __('stat.pruned') }}</dt>
             <dd>{{ $pruned_user }}</dd>
         </div>
         <div class="key-value__group">
-            <dt>{{ __('stat.banned') }} {{ __('common.users') }}</dt>
+            <dt>{{ __('stat.banned') }}</dt>
             <dd>{{ $banned_user }}</dd>
         </div>
-        <div class="key-value__group">
-            <dt>Users active today</dt>
-            <dd>{{ $users_active_today }}</dd>
-        </div>
-        <div class="key-value__group">
-            <dt>Users active this week</dt>
-            <dd>{{ $users_active_this_week }}</dd>
-        </div>
-        <div class="key-value__group">
-            <dt>Users active this month</dt>
-            <dd>{{ $users_active_this_month }}</dd>
+        <div class="key-value__group--nested">
+            <dt>{{ __('stat.active') }}</dt>
+            <dd>
+                <dl class="key-value">
+                    <div class="key-value__group">
+                        <dt>Today</dt>
+                        <dd>{{ $users_active_today }}</dd>
+                    </div>
+                    <div class="key-value__group">
+                        <dt>This week</dt>
+                        <dd>{{ $users_active_this_week }}</dd>
+                    </div>
+                    <div class="key-value__group">
+                        <dt>This month</dt>
+                        <dd>{{ $users_active_this_month }}</dd>
+                    </div>
+                    <div class="key-value__group">
+                        <dt>{{ __('stat.all') }}</dt>
+                        <dd>{{ $active_user }}</dd>
+                    </div>
+                </dl>
+            </dd>
         </div>
     </dl>
 </section>
