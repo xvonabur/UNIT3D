333247ed769b2cf48d909b85e3f10b569f4121e7
Roardom <roardom@protonmail.com>
2025-06-07 21:14:03 +0000

    fix: tmdb person cache
    Only cache the people after they've been upserted.

--- app/Jobs/ProcessTvJob.php
+++ app/Jobs/ProcessTvJob.php
@@ -117,6 +117,7 @@ class ProcessTvJob implements ShouldQueue
 
         $credits = $tvScraper->getCredits();
         $people = [];
+        $cache = [];
 
         foreach (array_unique(array_column($credits, 'tmdb_person_id')) as $personId) {
             // TMDB caches their api responses for 8 hours, so don't abuse them
@@ -127,12 +128,15 @@ class ProcessTvJob implements ShouldQueue
                 continue;
             }
 
-            cache()->put($cacheKey, now(), 8 * 3600);
-
             $people[] = (new Client\Person($personId))->getPerson();
+
+            $cache[$cacheKey] = now();
         }
 
         TmdbPerson::upsert($people, 'id');
+
+        cache()->put($cache, 8 * 3600);
+
         TmdbCredit::where('tmdb_tv_id', '=', $this->id)->delete();
         TmdbCredit::upsert($credits, ['tmdb_person_id', 'tmdb_movie_id', 'tmdb_tv_id', 'occupation_id', 'character']);
 
