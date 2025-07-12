HDInnovations/UNIT3D-Community-Edition

update: use only orderable meilisearch filters when necessary
Should improve indexing performance.

Depends on upstream meilisearch change scheduled for v1.14: https://www.github.com/meilisearch/meilisearch/issues/5163

config/scout.php
@@ -146,76 +146,98 @@ return [
                     'resolution.name',
                 ],
                 'filterableAttributes' => [
-                    'id',
-                    'name',
-                    'folder',
-                    'size',
-                    'leechers',
-                    'seeders',
-                    'times_completed',
-                    'created_at',
-                    'bumped_at',
-                    'fl_until',
-                    'du_until',
-                    'user_id',
-                    'imdb',
-                    'tvdb',
-                    'tmdb',
-                    'mal',
-                    'igdb',
-                    'season_number',
-                    'episode_number',
-                    'stream',
-                    'free',
-                    'doubleup',
-                    'refundable',
-                    'highspeed',
-                    'featured',
-                    'status',
-                    'anon',
-                    'sticky',
-                    'sd',
-                    'internal',
-                    'deleted_at',
-                    'personal_release',
-                    'trumpable',
-                    'info_hash',
-                    'history_seeders.user_id',
-                    'history_leechers.user_id',
-                    'history_active.user_id',
-                    'history_inactive.user_id',
-                    'history_complete.user_id',
-                    'history_incomplete.user_id',
-                    'user.username',
-                    'category.id',
-                    'category.movie_meta',
-                    'category.tv_meta',
-                    'type.id',
-                    'resolution.id',
-                    'movie.id',
-                    'movie.name',
-                    'movie.year',
-                    'movie.original_language',
-                    'movie.adult',
-                    'movie.genres.id',
-                    'movie.collection.id',
-                    'movie.companies.id',
-                    'movie.wishes.user_id',
-                    'tv.id',
-                    'tv.name',
-                    'tv.year',
-                    'tv.original_language',
-                    'tv.genres.id',
-                    'tv.networks.id',
-                    'tv.companies.id',
-                    'tv.wishes.user_id',
-                    'playlists.id',
-                    'bookmarks.user_id',
-                    'freeleech_tokens.user_id',
-                    'files.name',
-                    'keywords',
-                    'distributor_id',
-                    'region_id',
+                    [
+                        'attributePatterns' => [
+                            'id',
+                            'name',
+                            'folder',
+                            'leechers',
+                            'seeders',
+                            'bumped_at',
+                            'fl_until',
+                            'du_until',
+                            'user_id',
+                            'imdb',
+                            'tvdb',
+                            'tmdb',
+                            'mal',
+                            'igdb',
+                            'season_number',
+                            'episode_number',
+                            'stream',
+                            'free',
+                            'doubleup',
+                            'refundable',
+                            'highspeed',
+                            'featured',
+                            'status',
+                            'anon',
+                            'sticky',
+                            'sd',
+                            'internal',
+                            'deleted_at',
+                            'personal_release',
+                            'trumpable',
+                            'info_hash',
+                            'history_seeders.user_id',
+                            'history_leechers.user_id',
+                            'history_active.user_id',
+                            'history_inactive.user_id',
+                            'history_complete.user_id',
+                            'history_incomplete.user_id',
+                            'user.username',
+                            'category.id',
+                            'category.movie_meta',
+                            'category.tv_meta',
+                            'type.id',
+                            'resolution.id',
+                            'movie.id',
+                            'movie.name',
+                            'movie.original_language',
+                            'movie.adult',
+                            'movie.genres.id',
+                            'movie.collection.id',
+                            'movie.companies.id',
+                            'movie.wishes.user_id',
+                            'tv.id',
+                            'tv.name',
+                            'tv.original_language',
+                            'tv.genres.id',
+                            'tv.networks.id',
+                            'tv.companies.id',
+                            'tv.wishes.user_id',
+                            'playlists.id',
+                            'bookmarks.user_id',
+                            'freeleech_tokens.user_id',
+                            'files.name',
+                            'keywords',
+                            'distributor_id',
+                            'region_id',
+                        ],
+                        "features" => [
+                            "facetSearch" => false,
+                            "filter"      => [
+                                "equality"   => true,
+                                "comparison" => false,
+                            ],
+                        ]
+                    ],
+                    [
+                        'attributePatterns' => [
+                            'size',
+                            'times_completed',
+                            'created_at',
+                            'movie.year',
+                            'tv.year',
+                        ],
+                        "features" => [
+                            "facetSearch" => false,
+                            "filter"      => [
+                                "equality"   => true,
+                                "comparison" => true,
+                            ],
+                        ]
+                    ],
                 ],
                 'sortableAttributes' => [
                     'name',

xvonabur/UNIT3D

config/scout.php
@@ -146,75 +146,97 @@ return [
                     'resolution.name',
                 ],
                 'filterableAttributes' => [
-                    'id',
-                    'name',
-                    'folder',
-                    'size',
-                    'leechers',
-                    'seeders',
-                    'times_completed',
-                    'created_at',
-                    'bumped_at',
-                    'fl_until',
-                    'du_until',
-                    'user_id',
-                    'imdb',
-                    'tvdb',
-                    'tmdb_movie_id',
-                    'tmdb_tv_id',
-                    'mal',
-                    'igdb',
-                    'season_number',
-                    'episode_number',
-                    'free',
-                    'doubleup',
-                    'refundable',
-                    'highspeed',
-                    'featured',
-                    'status',
-                    'anon',
-                    'sticky',
-                    'internal',
-                    'deleted_at',
-                    'personal_release',
-                    'trumpable',
-                    'info_hash',
-                    'history_seeders.user_id',
-                    'history_leechers.user_id',
-                    'history_active.user_id',
-                    'history_inactive.user_id',
-                    'history_complete.user_id',
-                    'history_incomplete.user_id',
-                    'user.username',
-                    'category.id',
-                    'category.movie_meta',
-                    'category.tv_meta',
-                    'type.id',
-                    'resolution.id',
-                    'tmdb_movie.id',
-                    'tmdb_movie.name',
-                    'tmdb_movie.year',
-                    'tmdb_movie.original_language',
-                    'tmdb_movie.adult',
-                    'tmdb_movie.genres.id',
-                    'tmdb_movie.collection.id',
-                    'tmdb_movie.companies.id',
-                    'tmdb_movie.wishes.user_id',
-                    'tmdb_tv.id',
-                    'tmdb_tv.name',
-                    'tmdb_tv.year',
-                    'tmdb_tv.original_language',
-                    'tmdb_tv.genres.id',
-                    'tmdb_tv.networks.id',
-                    'tmdb_tv.companies.id',
-                    'tmdb_tv.wishes.user_id',
-                    'playlists.id',
-                    'bookmarks.user_id',
-                    'freeleech_tokens.user_id',
-                    'files.name',
-                    'keywords',
-                    'distributor_id',
-                    'region_id',
+                    [
+                        'attributePatterns' => [
+                            'id',
+                            'name',
+                            'folder',
+                            'leechers',
+                            'seeders',
+                            'bumped_at',
+                            'fl_until',
+                            'du_until',
+                            'user_id',
+                            'imdb',
+                            'tvdb',
+                            'tmdb_movie_id',
+                            'tmdb_tv_id',
+                            'mal',
+                            'igdb',
+                            'season_number',
+                            'episode_number',
+                            'free',
+                            'doubleup',
+                            'refundable',
+                            'highspeed',
+                            'featured',
+                            'status',
+                            'anon',
+                            'sticky',
+                            'internal',
+                            'deleted_at',
+                            'personal_release',
+                            'trumpable',
+                            'info_hash',
+                            'history_seeders.user_id',
+                            'history_leechers.user_id',
+                            'history_active.user_id',
+                            'history_inactive.user_id',
+                            'history_complete.user_id',
+                            'history_incomplete.user_id',
+                            'user.username',
+                            'category.id',
+                            'category.movie_meta',
+                            'category.tv_meta',
+                            'type.id',
+                            'resolution.id',
+                            'tmdb_movie.id',
+                            'tmdb_movie.name',
+                            'tmdb_movie.original_language',
+                            'tmdb_movie.adult',
+                            'tmdb_movie.genres.id',
+                            'tmdb_movie.collection.id',
+                            'tmdb_movie.companies.id',
+                            'tmdb_movie.wishes.user_id',
+                            'tmdb_tv.id',
+                            'tmdb_tv.name',
+                            'tmdb_tv.original_language',
+                            'tmdb_tv.genres.id',
+                            'tmdb_tv.networks.id',
+                            'tmdb_tv.companies.id',
+                            'tmdb_tv.wishes.user_id',
+                            'playlists.id',
+                            'bookmarks.user_id',
+                            'freeleech_tokens.user_id',
+                            'files.name',
+                            'keywords',
+                            'distributor_id',
+                            'region_id',
+                        ],
+                        "features" => [
+                            "facetSearch" => false,
+                            "filter"      => [
+                                "equality"   => true,
+                                "comparison" => false,
+                            ],
+                        ]
+                    ],
+                    [
+                        'attributePatterns' => [
+                            'size',
+                            'times_completed',
+                            'created_at',
+                            'tmdb_movie.year',
+                            'tmdb_tv.year',
+                        ],
+                        "features" => [
+                            "facetSearch" => false,
+                            "filter"      => [
+                                "equality"   => true,
+                                "comparison" => true,
+                            ],
+                        ]
+                    ],
                 ],
                 'sortableAttributes' => [
                     'name',